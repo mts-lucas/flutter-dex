dcb09714356b8a379ae678956e197b4eec590f85
Lucas Mateus <lmateus1067@outlook.com>
2023-05-24 14:20:54 -0300

    feat: add fire, water, grass e eletric #3

---
+++ flutter_dex/lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TypeColors {
+  Color fire = Colors.orange;
+  Color water = Colors.blue;
+  Color grass = Colors.green;
+  Color eletric = Colors.yellowAccent;
+}

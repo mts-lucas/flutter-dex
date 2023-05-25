cd1f8255a84180992fc7f7b932c70cf421395ef2
Lucas Mateus <lmateus1067@outlook.com>
2023-05-24 21:42:34 -0300

    feat: estilizando navbar #5

--- flutter_dex/lib/components/navbar.dart
+++ flutter_dex/lib/components/navbar.dart
@@ -1,25 +1,52 @@
 import 'package:flutter/material.dart';
+import '../utils/colors.dart';
 
 class DexAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      shape: const CircularNotchedRectangle(),
-      color: Colors.red,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          IconButton(
-            tooltip: 'Open Area Module',
-            icon: const Icon(Icons.map_outlined),
-            onPressed: () {},
-          ),
-          IconButton(
-            tooltip: 'Favorite',
-            icon: const Icon(Icons.circle),
-            onPressed: () {},
-          ),
-        ],
+    return ClipRRect(
+      borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(10.0)), // Define o raio de curvatura desejado
+      child: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        color: AppColors.bar,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              height: 46.0,
+              width: 100.0, // Ajuste a largura desejada
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(
+                    15.0), // Define o raio de curvatura das bordas
+                color: AppColors.background,
+              ),
+              child: IconButton(
+                tooltip: 'Open Regions Module',
+                icon: const Icon(Icons.map_outlined),
+                color: Colors.white,
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              height: 46.0,
+              width: 100.0, // Ajuste a largura desejada
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(
+                    15.0), // Define o raio de curvatura das bordas
+                color: AppColors.background,
+              ),
+              child: IconButton(
+                tooltip: 'Open Quiz Module',
+                icon: const Icon(Icons.quiz_outlined),
+                color: AppColors.text,
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

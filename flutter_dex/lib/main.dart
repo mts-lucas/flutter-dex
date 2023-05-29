1c4b48e28533334103d17e2e3c29f7e0d3158fcf
Lucas Mateus <lmateus1067@outlook.com>
2023-05-29 16:14:01 -0300

    feat: add bigloading #18

--- flutter_dex/lib/main.dart
+++ flutter_dex/lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dex/homepage/homePage.dart';
 import 'package:flutter_dex/quiz/telaQuiz.dart';
 import 'package:flutter_dex/regions/telaRegion.dart';
+// import 'package:flutter_dex/utils/loading.dart';
+// import 'package:flutter_dex/components/appBar.dart';
+// import 'package:flutter_dex/components/homeButtom.dart';
+// import 'package:flutter_dex/components/bottomNavBar.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,3 +30,18 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// class TelaTeste extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: UpAppBar(),
+//       body: Center(
+//         child: BigLoading(),
+//       ),
+//       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+//       floatingActionButton: DexButtomHome(),
+//       bottomNavigationBar: BottomDexAppBar(),
+//     );
+//   }
+// }
---
+++ flutter_dex/lib/utils/loading.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'colors.dart';
+
+class BigLoading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SpinKitFadingCircle(
+      color: AppColors.effectsBlue,
+      size: 200.0,
+    );
+  }
+}

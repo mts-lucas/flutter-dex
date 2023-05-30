ef39375c73d9cd9290e2514c47e7495e86d2eeb8
Lucas Mateus <lmateus1067@outlook.com>
2023-05-30 10:23:45 -0300

    feat: add sapacebox #25

--- flutter_dex/lib/detail/datailPokemon.dart
+++ flutter_dex/lib/detail/datailPokemon.dart
@@ -14,6 +14,7 @@ class DetailPokemon extends StatelessWidget {
               PkmImg(),
               PkmNameIndex(),
               PkmTypes(),
+              SizedBox(height: 10,),
               PkmStats(),
               SizedBox(height: 40,),
             ],

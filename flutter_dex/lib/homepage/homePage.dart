f1b96e33e65223c02c70ba7a60fdfd88f6c75d70
Lucas Mateus <lmateus1067@outlook.com>
2023-05-31 14:31:48 -0300

    refactor: mundaca de tela #33

--- flutter_dex/lib/homepage/homePage.dart
+++ flutter_dex/lib/homepage/homePage.dart
@@ -8,6 +8,7 @@ import '../components/appBar.dart';
 import '../utils/colors.dart';
 import '../utils/loading.dart';
 import '../utils/captalize.dart';
+import '../detail/pkmView.dart';
 
 class DataService {
   final ValueNotifier<List> tableStateNotifier = ValueNotifier([]);
@@ -107,7 +108,7 @@ class MyCardWidget extends HookWidget {
   final Function() scrollEndedCallback;
 
   const MyCardWidget(
-      {super.key, required this.objects , required this.scrollEndedCallback});
+      {super.key, required this.objects, required this.scrollEndedCallback});
 
   @override
   Widget build(BuildContext context) {
@@ -149,6 +150,9 @@ class MyCardWidget extends HookWidget {
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   ListTile(
+                    onTap: () {
+                      Navigator.pushNamed(context, 'teladetail');
+                    },
                     leading: Container(
                       width: 100,
                       height: 300,

40fcd8967756e3ffbbd49fefbaad685180970b93
Lucas Mateus <lmateus1067@outlook.com>
2023-06-01 20:08:38 -0300

    feat: add first prototype Criar Tela Quiz #36

--- flutter_dex/lib/search/SearchInput.dart
+++ flutter_dex/lib/search/SearchInput.dart
@@ -6,7 +6,13 @@ import '../utils/colors.dart';
 import '../utils/dexfonts.dart';
 
 class SearchInput extends StatelessWidget {
-  dynamic jsonObject;
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,23 +21,50 @@ class SearchInput extends StatelessWidget {
         padding: EdgeInsets.all(16),
         child: Theme(
           data: ThemeData(
-            primaryColor: AppColors.text, // Cor das linhas externas e texto
-            hintColor: AppColors.text, // Cor do texto de dica
+            primaryColor: AppColors.text,
+            hintColor: AppColors.text,
             inputDecorationTheme: InputDecorationTheme(
               filled: true,
-              fillColor: AppColors.second, // Cor de fundo
+              fillColor: AppColors.second,
               border: OutlineInputBorder(
-                borderSide: BorderSide(
-                    color: AppColors.text), // Cor das linhas externas
+                borderSide: BorderSide(color: AppColors.text),
               ),
             ),
           ),
           child: Form(
-            child: TextFormField(
-              style: DexFont(fontSize: 16.0).normal(),
-              decoration: InputDecoration(
-                labelText: 'Search your Pokemon by name',
-              ),
+            key: _formKey,
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: _textEditingController,
+                    style: DexFont(fontSize: 16.0).normal(),
+                    decoration: InputDecoration(
+                      labelText: 'Search your Pokemon by name',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                    width:
+                        10), // Espaçamento entre a barra de pesquisa e o botão
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      String searchQuery = _textEditingController.text;
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: Text('Processing Data: $searchQuery')),
+                      );
+                    },
+                    child: Icon(Icons.search),
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(15),
+                      minimumSize: Size(60, 60),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),

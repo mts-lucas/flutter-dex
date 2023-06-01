6f1c6094a7020e1fd9abd4e43f7e564eb853d8dc
Lucas Mateus <lmateus1067@outlook.com>
2023-06-01 20:20:29 -0300

    feat: final model #36

--- flutter_dex/lib/search/SearchInput.dart
+++ flutter_dex/lib/search/SearchInput.dart
@@ -19,54 +19,62 @@ class SearchInput extends StatelessWidget {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(16),
-        child: Theme(
-          data: ThemeData(
-            primaryColor: AppColors.text,
-            hintColor: AppColors.text,
-            inputDecorationTheme: InputDecorationTheme(
-              filled: true,
-              fillColor: AppColors.second,
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: AppColors.text),
+        child: Column(
+          children: [
+            Image.asset('assets/images/dexlogo.png', scale: 1.3,), // Imagem centralizada
+            SizedBox(
+                height:
+                    60), // Espaçamento entre a imagem e o restante do conteúdo
+            Theme(
+              data: ThemeData(
+                primaryColor: AppColors.text,
+                hintColor: AppColors.text,
+                inputDecorationTheme: InputDecorationTheme(
+                  filled: true,
+                  fillColor: AppColors.second,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: AppColors.text),
+                  ),
+                ),
+              ),
+              child: Form(
+                key: _formKey,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _textEditingController,
+                        style: DexFont(fontSize: 16.0).normal(),
+                        decoration: InputDecoration(
+                          labelText: 'Search your Pokemon by name',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                        width:
+                            10), // Espaçamento entre a barra de pesquisa e o botão
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          String searchQuery = _textEditingController.text;
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                                content: Text('Processing Data: $searchQuery')),
+                          );
+                        },
+                        child: Icon(Icons.search),
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.all(15),
+                          minimumSize: Size(60, 60),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          child: Form(
-            key: _formKey,
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: _textEditingController,
-                    style: DexFont(fontSize: 16.0).normal(),
-                    decoration: InputDecoration(
-                      labelText: 'Search your Pokemon by name',
-                    ),
-                  ),
-                ),
-                SizedBox(
-                    width:
-                        10), // Espaçamento entre a barra de pesquisa e o botão
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      String searchQuery = _textEditingController.text;
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                            content: Text('Processing Data: $searchQuery')),
-                      );
-                    },
-                    child: Icon(Icons.search),
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.all(15),
-                      minimumSize: Size(60, 60),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          ],
         ),
       ),
     );

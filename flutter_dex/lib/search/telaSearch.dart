5a6be378cf3ef132ba63259193dc2e46f10d8f95
Lucas Mateus <lmateus1067@outlook.com>
2023-06-01 14:28:54 -0300

    refactor: module #36

--- flutter_dex/lib/search/telaSearch.dart
+++ flutter_dex/lib/search/telaSearch.dart
@@ -2,26 +2,8 @@ import 'package:flutter/material.dart';
 import '../components/bottomNavBar.dart';
 import '../components/homeButtom.dart';
 import '../components/appBar.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import '../utils/search.dart';
 
-class DataSearch {
-  final ValueNotifier<List> tableStateNotifier = ValueNotifier([]);
-
-  Future<void> loadPokemon(String pkm) async {
-    var pokeUri = Uri(
-      scheme: 'https',
-      host: 'pokeapi.co',
-      path: 'api/v2/pokemon/${pkm}',
-    );
-
-    var jsonString = await http.read(pokeUri);
-
-    var pokeJson = jsonDecode(jsonString);
-
-    tableStateNotifier.value = pokeJson; // Convert pokeJson to a List
-  }
-}
 
 final dataSearch = DataSearch();
 
---
+++ flutter_dex/lib/utils/search.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class DataSearch {
+  final ValueNotifier<List> tableStateNotifier = ValueNotifier([]);
+
+  Future<void> loadPokemon(var pkm) async {
+    var pokeUri = Uri(
+      scheme: 'https',
+      host: 'pokeapi.co',
+      path: 'api/v2/pokemon/${pkm}',
+    );
+
+    var jsonString = await http.read(pokeUri);
+
+    var pokeJson = jsonDecode(jsonString);
+
+    tableStateNotifier.value = pokeJson; // Convert pokeJson to a List
+  }
+}

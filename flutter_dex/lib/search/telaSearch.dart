88996cf0b12b9515e8d9fa00f1e2a5260b29fb95
Lucas Mateus <lmateus1067@outlook.com>
2023-06-01 14:19:04 -0300

    feat: add requisition #36

--- flutter_dex/lib/search/telaSearch.dart
+++ flutter_dex/lib/search/telaSearch.dart
@@ -2,6 +2,28 @@ import 'package:flutter/material.dart';
 import '../components/bottomNavBar.dart';
 import '../components/homeButtom.dart';
 import '../components/appBar.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class DataSearch {
+  final ValueNotifier<List> tableStateNotifier = ValueNotifier([]);
+
+  Future<void> loadPokemon(String pkm) async {
+    var pokeUri = Uri(
+      scheme: 'https',
+      host: 'pokeapi.co',
+      path: 'api/v2/pokemon/${pkm}',
+    );
+
+    var jsonString = await http.read(pokeUri);
+
+    var pokeJson = jsonDecode(jsonString);
+
+    tableStateNotifier.value = pokeJson; // Convert pokeJson to a List
+  }
+}
+
+final dataSearch = DataSearch();
 
 class TelaSearch extends StatelessWidget {
   @override

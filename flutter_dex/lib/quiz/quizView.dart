2cc1659db8c197c136f938c87dab957957699c0f
Lucas Mateus <lmateus1067@outlook.com>
2023-06-02 22:23:31 -0300

    refactor: dynamic img

--- flutter_dex/lib/quiz/quizView.dart
+++ flutter_dex/lib/quiz/quizView.dart
@@ -1,20 +1,102 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dex/search/searchInput.dart';
+import 'package:flutter_dex/utils/loading.dart';
+import 'package:flutter_dex/utils/search.dart';
 import '../components/bottomNavBar.dart';
 import '../components/homeButtom.dart';
 import '../components/appBar.dart';
+import '../utils/dexfonts.dart';
+import 'dart:math';
 
+DataSearch dataSearch = DataSearch();
 
+class QuizContent extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _textEditingController = TextEditingController();
+  Random randomize = new Random();
+  dynamic myPokemon;
 
-class TelaSearch extends StatelessWidget {
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: UpAppBar(),
-      body: SearchInput(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: DexButtomHome(),
-      bottomNavigationBar: BottomDexAppBar(),
+    dataSearch.loadPokemon(randomize.nextInt(1009));
+    return ValueListenableBuilder(
+      valueListenable: dataSearch.tableStateNotifier,
+      builder: (_, value, __) {
+        myPokemon = value;
+        if (myPokemon['status'] != 'ready') {
+          return BigLoading();
+        } else {
+          return Center(
+            child: Container(
+              child: Stack(
+                children: [
+                  Image.network(
+                    myPokemon['result']['sprites']['other']['official-artwork']
+                        ['front_default'],
+                    fit: BoxFit.cover,
+                    width: 200, // Ajuste o tamanho conforme necessário
+                    height: 200,
+                    // opacity: AlwaysStoppedAnimation(0.1),
+                    color: Colors.white,
+                  ),
+                  // Máscara preta
+                  Form(
+                    key: _formKey,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            controller: _textEditingController,
+                            style: DexFont(fontSize: 16.0).normal(),
+                            decoration: InputDecoration(
+                              labelText: 'Try to get the right name',
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(30),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              String searchQuery = _textEditingController.text;
+                              if (myPokemon['result']['name'] == searchQuery) {
+                                print('acertou mizeravi');
+                              } else {
+                                final snackBar = SnackBar(
+                                  content: Text(
+                                      '${searchQuery} is Not correct Pokemon'),
+                                  behavior: SnackBarBehavior.floating,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                );
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(snackBar);
+                              }
+                            },
+                            child: Icon(Icons.search),
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.all(15),
+                              minimumSize: Size(60, 60),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
--- flutter_dex/lib/quiz/telaQuiz.dart
+++ flutter_dex/lib/quiz/telaQuiz.dart
@@ -4,39 +4,16 @@ import 'package:flutter/material.dart';
 import '../components/bottomNavBar.dart';
 import '../components/homeButtom.dart';
 import '../components/appBar.dart';
-// import '../utils/search.dart';
+import './quizView.dart';
 
-// final DataSearch dataSearch = DataSearch();
 
 class TelaQuiz extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final jsonObject = dataSearch.tableStateNotifier.value;
-    // final imgUrl = jsonObject['result']['sprites']['other']['official-artwork']
-    //     ['front_default'];
+
     return Scaffold(
       appBar: UpAppBar(),
-      body: Center(
-          child: Container(
-        child: Stack(
-          children: [
-            Image.network(
-              'assets/images/dexlogo.png',
-              fit: BoxFit.cover,
-              width: 200, // Ajuste o tamanho conforme necessário
-              height: 200,
-              // opacity: AlwaysStoppedAnimation(0.1),
-              color: Colors.white,
-            ),
-            // Máscara preta
-            Container(
-              color: Colors.white.withOpacity(0), // Define a opacidade desejada
-              width: 200, // Ajuste o tamanho conforme necessário
-              height: 200,
-            ),
-          ],
-        ),
-      )),
+      body: QuizContent(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: DexButtomHome(),
       bottomNavigationBar: BottomDexAppBar(),

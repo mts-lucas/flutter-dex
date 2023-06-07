e831c9cc8be9ad9804c9646b464131a94831dbbe
Lucas Mateus <lmateus1067@outlook.com>
2023-06-07 13:37:03 -0300

    refactor: rename archive #53

--- flutter_dex/lib/components/bottomNavBar.dart
+++ flutter_dex/lib/components/bottomNavBar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../utils/colors.dart';
-import 'buttomRegions.dart';
+import 'buttomSearch.dart';
 import 'buttomQuiz.dart';
 
 class BottomDexAppBar extends StatelessWidget {

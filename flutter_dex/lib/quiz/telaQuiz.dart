1a59f9ee8e3416e443e7e89f995d281d1bbb3654
Lucas Mateus <lmateus1067@outlook.com>
2023-06-14 14:10:50 -0300

    refactor: utils, search and quiz #56

--- flutter_dex/lib/quiz/telaQuiz.dart
+++ flutter_dex/lib/quiz/telaQuiz.dart
@@ -1,16 +1,12 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import '../components/bottomNavBar.dart';
 import '../components/homeButtom.dart';
 import '../components/appBar.dart';
 import './quizView.dart';
 
-
 class TelaQuiz extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: UpAppBar(text: 'POKEMON QUIZ'),
       body: QuizContent(),
